f6836b6c52f366fc31d48bf6d6561cad7e91cd78
Erica <eri8d@hotmail.com>
2019-02-21 11:43:11 -0500

    Added multiple application screens.
    Created dashboard, pet profile, edit pet profile, user profile, edit user profile, and reminder activities.

---
+++ app/src/main/java/com/alanmbennett/petcare/User_Profile.java
@@ -0,0 +1,21 @@
+package com.alanmbennett.petcare;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class User_Profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user__profile);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        
+    }
+
+}
