a67cb45b23439c40c9907ced34fe441c9f942e71
Alan B <alanmitchellbennett@gmail.com>
2019-02-20 20:10:15 -0500

    Added AddPetActivity and added functionality to switch from SignupActivity to AddPetActivity

--- app/src/main/java/com/alanmbennett/petcare/SignupActivity.java
+++ app/src/main/java/com/alanmbennett/petcare/SignupActivity.java
@@ -1,7 +1,10 @@
 package com.alanmbennett.petcare;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class SignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        Button signup = (Button) this.findViewById(R.id.signup_act_button);
+
+        signup.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                switchToAddPet(view);
+            }
+        });
+    }
+
+    public void switchToAddPet(View view)
+    {
+        Intent intent= new Intent(this, AddPetActivity.class);
+        startActivity(intent);
     }
 }
