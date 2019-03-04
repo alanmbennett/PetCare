77658ee9ab337d1c46f367ff541982224a9f055f
Erica <eri8d@hotmail.com>
2019-03-04 01:34:50 -0500

    Added UI for add reminder activity.

--- app/src/main/java/com/alanmbennett/petcare/AddReminderActivity.java
+++ app/src/main/java/com/alanmbennett/petcare/AddReminderActivity.java
@@ -1,14 +1,17 @@
 package com.alanmbennett.petcare;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class AddReminderActivity extends AppCompatActivity {
 
+    Button createReminder;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +20,12 @@ public class AddReminderActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        createReminder = (Button) this.findViewById(R.id.create_reminder_button);
+        createReminder.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(AddReminderActivity.this, DashboardActivity.class));
+            }
+        });
     }
 
 }
