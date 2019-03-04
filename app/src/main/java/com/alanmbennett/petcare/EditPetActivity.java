f07be44411c5b8dae930fe9f7e3999b596b7bfa1
Erica <eri8d@hotmail.com>
2019-03-04 09:40:36 -0500

    Added ui to edit pet screen

--- app/src/main/java/com/alanmbennett/petcare/EditPetActivity.java
+++ app/src/main/java/com/alanmbennett/petcare/EditPetActivity.java
@@ -1,13 +1,16 @@
 package com.alanmbennett.petcare;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class EditPetActivity extends AppCompatActivity {
+    Button editPet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,15 +19,16 @@ public class EditPetActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        editPet = (Button) this.findViewById(R.id.btEditPet);
+        editPet.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                startActivity(new Intent(EditPetActivity.this, PetProfileActivity.class));
             }
         });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 
 }
