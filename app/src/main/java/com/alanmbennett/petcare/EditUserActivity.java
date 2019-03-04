f79db06e8b1afd5953a6237a53d707e63b3c0f84
Erica <eri8d@hotmail.com>
2019-03-04 09:26:10 -0500

    Added ui to edit user

--- app/src/main/java/com/alanmbennett/petcare/EditUserActivity.java
+++ app/src/main/java/com/alanmbennett/petcare/EditUserActivity.java
@@ -1,14 +1,16 @@
 package com.alanmbennett.petcare;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class EditUserActivity extends AppCompatActivity {
-
+    Button editUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +18,18 @@ public class EditUserActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        editUser = (Button) this.findViewById(R.id.btEditUser);
+        editUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(EditUserActivity.this, UserProfileActivity.class));
+            }
+        });
+
     }
+
+
 
 }
