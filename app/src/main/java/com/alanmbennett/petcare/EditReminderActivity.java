fecfa3e470a2af39736dd433521e8ca4f6f23292
Erica <eri8d@hotmail.com>
2019-03-04 10:07:58 -0500

    Edit reminder ui and small fix to reminder activity

--- app/src/main/java/com/alanmbennett/petcare/EditReminderActivity.java
+++ app/src/main/java/com/alanmbennett/petcare/EditReminderActivity.java
@@ -1,14 +1,17 @@
 package com.alanmbennett.petcare;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class EditReminderActivity extends AppCompatActivity {
 
+    Button editReminder;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +20,15 @@ public class EditReminderActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        editReminder= (Button) this.findViewById(R.id.btEditReminder);
+        editReminder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(EditReminderActivity.this, ReminderActivity.class));
+            }
+        });
+
     }
 
 }
