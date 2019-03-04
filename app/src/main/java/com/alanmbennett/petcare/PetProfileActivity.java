8a1ba801bcb43640362648c4cc80d80c75ecde7f
Erica <eri8d@hotmail.com>
2019-03-04 01:57:44 -0500

    Added UI elements to pet profile page and activities

--- app/src/main/java/com/alanmbennett/petcare/PetProfileActivity.java
+++ app/src/main/java/com/alanmbennett/petcare/PetProfileActivity.java
@@ -1,14 +1,16 @@
 package com.alanmbennett.petcare;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class PetProfileActivity extends AppCompatActivity {
-
+    Button editPet;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +22,15 @@ public class PetProfileActivity extends AppCompatActivity {
             public void onClick(View v) {
                 // back button pressed
                 onBackPressed();
+            }
+        });
+
+
+        editPet = (Button) this.findViewById(R.id.btEditPet);
+        editPet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(PetProfileActivity.this, EditPetActivity.class));
             }
         });
         
