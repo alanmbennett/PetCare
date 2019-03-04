64412d26b10f441f82b351bb6d5e79606b92f1d3
Erica <eri8d@hotmail.com>
2019-03-03 22:50:12 -0500

    Update to profiles

--- app/src/main/java/com/alanmbennett/petcare/PetProfileActivity.java
+++ app/src/main/java/com/alanmbennett/petcare/PetProfileActivity.java
@@ -15,15 +15,7 @@ public class PetProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_pet__profile);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        
     }
 
 }
