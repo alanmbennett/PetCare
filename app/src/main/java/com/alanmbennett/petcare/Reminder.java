140e362f2d9a08d00564d6650d0bf884050f6991
Erica <eri8d@hotmail.com>
2019-03-26 23:45:29 -0400

    Created Reminder class.

---
+++ app/src/main/java/com/alanmbennett/petcare/Reminder.java
@@ -0,0 +1,57 @@
+package com.alanmbennett.petcare;
+
+public class Reminder {
+    private String title;
+    private String description;
+    private String time;
+    private String reoccuring;
+    private Integer petid;
+
+    public Reminder(String title, String description, String time, String reoccuring, Integer petid) {
+        this.title = title;
+        this.description = description;
+        this.time = time;
+        this.reoccuring = reoccuring;
+        this.petid = petid;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getReoccuring() {
+        return reoccuring;
+    }
+
+    public void setReoccuring(String reoccuring) {
+        this.reoccuring = reoccuring;
+    }
+
+    public Integer getPetid() {
+        return petid;
+    }
+
+    public void setPetid(Integer petid) {
+        this.petid = petid;
+    }
+}
